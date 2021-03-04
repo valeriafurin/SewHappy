36059584b9fcebef545f83b4f21a3b0ca07634df
valeriafurin <v.furin@web.de>
2021-03-04 15:28:32 +0100

    Added Button file

---
+++ src/styled-components/Button.styled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 220px;
+  height: 50px;
+  margin-top: 10px;
+  margin-left: 1rem;
+  border: none;
+  outline: none;
+  color: #fff;
+  background: rgba(98, 164, 171, 0.75);
+  cursor: pointer;
+  position: relative;
+  border-radius: 10px;
+`;
