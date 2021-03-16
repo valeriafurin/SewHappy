df626bb1382bee095ce0cafc7c294d99a7dd14e6
valeriafurin <v.furin@web.de>
2021-03-16 15:29:45 +0100

    Added styling for Burger Menu + open animnation

---
+++ src/components/styled-components/Burger.styled.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const StyledBurger = styled.button<{ open?: boolean }>`
+  display: none;
+  color: grey;
+
+  @media screen and (max-width: 768px) {
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: flex;
+    font-size: 1.8rem;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 2rem;
+    height: 2rem;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    z-index: 10;
+
+    &:focus {
+      outline: none;
+    }
+
+    div {
+      width: 2rem;
+      height: 0.25rem;
+      background: ${({ open }) => (open ? "#0D0C1D" : "#EFFFFA")};
+      border-radius: 10px;
+      transition: all 0.3s linear;
+      position: relative;
+      transform-origin: 1px;
+
+      :first-child {
+        transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+      }
+
+      :nth-child(2) {
+        opacity: ${({ open }) => (open ? "0" : "1")};
+        transform: ${({ open }) =>
+          open ? "translateX(20px)" : "translateX(0)"};
+      }
+
+      :nth-child(3) {
+        transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+      }
+    }
+  }
+`;
