6e1affed29b3add9c94fde6168f45c505e6312c0
valeriafurin <v.furin@web.de>
2021-04-21 20:51:25 +0200

    Created page for showcasing single articles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GlobalStyle } from "./GlobalStyle";
+import { GlobalStyle } from "./pages/pagesStyles/GlobalStyle.styled";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
@@ -15,16 +15,12 @@ function App() {
       <div>
         <Router>
           <Navbar />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/fabric-calculator" exact component={FabricCalculator} />
+          <Route path="/skirt-calculator" exact component={SkirtCalculator} />
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" exact component={About} />
-            <Route
-              path="/fabric-calculator"
-              exact
-              component={FabricCalculator}
-            />
-            <Route path="/skirt-calculator" exact component={SkirtCalculator} />
-            <Route path="/blog" exact component={Blog} />
+            <Route path="/blog" component={Blog} />
           </Switch>
         </Router>
       </div>
