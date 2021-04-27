dc1b138dce59a84ea337ab30183dc5fcef762676
valeriafurin <v.furin@web.de>
2021-04-27 13:46:55 +0200

    Adjusted Blog Posts styling

--- src/pages/pagesStyles/CardStyle.styled.tsx
+++ src/pages/pagesStyles/CardStyle.styled.tsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 export const CardWrapper = styled.div`
   overflow: hidden;
-  padding: 0 0 32px;
-  margin: 48px auto 0;
-  width: 300px;
+  width: 20em;
+  height: 25em;
+  margin: 10px 15px;
   font-family: Quicksand, arial, sans-serif;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
